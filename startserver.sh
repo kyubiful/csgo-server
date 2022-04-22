2cb660a92b3b0461c904832c17eaceed416df437
Kyubiful <ser.zabala@gmail.com>
2022-04-22 20:57:24 +0200

    2022

--- startserver.sh
+++ startserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./srcds_run -game csgo -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2 +setsteamaccount 7C933D72733239CFD536911B88ABDAD7 -tickrate 128 -port 27015 -pingboost 3 -exec server.cfg -maxplayers 12
+./srcds_run -game csgo -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2 +setsteamaccount 7C933D72733239CFD536911B88ABDAD7 -tickrate 128 -port 27015 -pingboost 3 -exec server.cfg -maxplayers 12 -addhltv1 1 -ip xxx.xxx.xxx.xxx
 
 # probar pingboost con sus tres distintos valores <1/2/3>
